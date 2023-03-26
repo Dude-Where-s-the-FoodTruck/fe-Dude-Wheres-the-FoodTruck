3915adc7d727c998263f0bbd4591d30c45c177f4
John Ammon <ammonj78@gmail.com>
2023-03-26 14:31:44 -0600

    Logo is displaying on App

--- foodtruck/src/components/Header/Header.tsx
+++ foodtruck/src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import headerLogo from '../../assets/foodtruck-logo.png'
 import './Header.css'
 
 export const Header = () => {
     return(
         <header>
             <h1>Dude, Where's My FoodTruck</h1>
-            {/* <img src={} alt></img> */}
+            <img src={headerLogo} alt='foodTruck logo'></img>
         </header>
     )
 }
