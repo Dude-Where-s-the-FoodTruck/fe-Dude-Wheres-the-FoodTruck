a89d384c0def93610cee3d6b4937483a7674f519
jordan smith <jordan.smith6035@gmail.com>
2023-03-27 07:57:04 -0600

    Add truck details component

---
+++ foodtruck/src/components/TruckDetails/TruckDetails.tsx
@@ -0,0 +1,22 @@
+import './TruckDetails.css'
+import React from "react";
+
+interface TruckDetailsProps {}
+
+class TruckDetails extends React.Component<TruckDetailsProps> {
+    constructor(props:TruckDetailsProps) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default TruckDetails
