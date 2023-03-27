4fe4699e2202e4565644308edb27d2d3a91b9299
Angie Staffieri <angie.staffieri@gmail.com>
2023-03-27 14:35:36 -0600

    Added main page and css styling

--- foodtruck/src/components/MainPage/MainPage.tsx
+++ foodtruck/src/components/MainPage/MainPage.tsx
@@ -1,12 +1,27 @@
 import './MainPage.css'
 import FilterForm from '../FilterForm/FilterForm'
+import { Trucks } from '../Trucks/Trucks';
 
+interface MainPageProps {
+    truckData: {
+        data: {
+            id: number;
+            attributes: {
+                name: string;
+                cuisine_type: string;
+                web_link: string;
+                image_link: string;
+            };
+        }[];
+    }[];
+}
 
-
-export const MainPage = () => {
+export const MainPage: React.FC<MainPageProps> = ({ truckData } ) => {
+    const trucksProps: TrucksProps = { truckData };
     return (
-        <div>
+        <div className='truck-view'>
             <FilterForm />
+            <Trucks {...trucksProps} />
         </div>
     )
 }
