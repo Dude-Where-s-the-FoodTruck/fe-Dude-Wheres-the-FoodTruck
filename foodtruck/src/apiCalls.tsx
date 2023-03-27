b1c6b38096fb13a6abedc7344bd89e0c87fb2d28
Angie Staffieri <angie.staffieri@gmail.com>
2023-03-27 15:19:19 -0600

    added city and date to dummy data

--- foodtruck/src/apiCalls.tsx
+++ foodtruck/src/apiCalls.tsx
@@ -4,6 +4,8 @@ export const dummyData = [
         {id: 1,
         attributes: {
           name: 'Meals on Wheels',
+          city: 'Denver',
+          date: '04/27/2023',
           cuisine_type: "American",
           web_link: "www.google.com",
           image_link: "https://i.etsystatic.com/14707052/r/il/00fc2c/1153843098/il_1140xN.1153843098_9jks.jpg"
@@ -16,6 +18,8 @@ export const dummyData = [
         {id: 2,
         attributes: {
           name: 'This is a Food Truck',
+          city: 'Colorado Springs',
+          date: '4/28/2023',
           cuisine_type: "Chinese",
           web_link: "www.google.com",
           image_link: "https://i.etsystatic.com/14707052/r/il/00fc2c/1153843098/il_1140xN.1153843098_9jks.jpg"
@@ -28,6 +32,8 @@ export const dummyData = [
         {id: 3,
         attributes: {
           name: 'Amigos Tacos',
+          city: 'Edgewater',
+          date: '04/29/2023',
           cuisine_type: "Mexican",
           web_link: "www.google.com",
           image_link: "https://i.etsystatic.com/14707052/r/il/00fc2c/1153843098/il_1140xN.1153843098_9jks.jpg"
@@ -40,6 +46,8 @@ export const dummyData = [
         {id: 4,
         attributes: {
           name: 'FBI Van FoodTruck',
+          city: 'Littleton',
+          date: '04/28/2023',
           cuisine_type: "Lies",
           web_link: "www.google.com",
           image_link: "https://i.etsystatic.com/14707052/r/il/00fc2c/1153843098/il_1140xN.1153843098_9jks.jpg"
@@ -52,6 +60,8 @@ export const dummyData = [
         {id: 5,
         attributes: {
           name: 'Here is the Food',
+          city: 'Fort Collins',
+          date: '04/30/2023',
           cuisine_type: "Hatian",
           web_link: "www.google.com",
           image_link: "https://i.etsystatic.com/14707052/r/il/00fc2c/1153843098/il_1140xN.1153843098_9jks.jpg"
