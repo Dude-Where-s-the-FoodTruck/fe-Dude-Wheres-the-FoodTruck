595bccc87ffd76845e93206feb5866a678e0b770
Angie Staffieri <angie.staffieri@gmail.com>
2023-03-27 14:39:36 -0600

    added truck and truck styling

--- foodtruck/src/components/Trucks/Trucks.tsx
+++ foodtruck/src/components/Trucks/Trucks.tsx
@@ -1,9 +1,38 @@
 import './Trucks.css'
+import { TruckCard } from '../TruckCard/TruckCard';
+
+export interface TrucksProps {
+    truckData: {
+        data: {
+            id: number;
+            attributes: {
+                name: string;
+                cuisine_type: string;
+                web_link: string;
+                image_link: string;
+            };
+        }[];
+    }[];
+}
 
 
-export const Trucks = () => {
+export const Trucks: React.FC<TrucksProps> = ({ truckData }) => {
+    const dataFix = truckData.map((truck) => truck.data[0])
+    const truckCards = dataFix.map((truck) => {
+        return(
+            <TruckCard 
+                id={truck.id}
+                name={truck.attributes.name}
+                cuisine={truck.attributes.cuisine_type}
+                website={truck.attributes.web_link}
+                image={truck.attributes.image_link}
+                key={truck.id}
+            />
+        )
+    })
     return (
-        <div>  
+        <div className='trucks-container'>
+              {truckCards}
         </div>
     )
 }
