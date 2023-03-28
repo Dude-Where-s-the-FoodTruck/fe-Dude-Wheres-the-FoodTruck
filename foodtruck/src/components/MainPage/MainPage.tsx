ffee42fb13d129614b77060e45cc4dc2e5ddb391
John Ammon <ammonj78@gmail.com>
2023-03-28 16:00:07 -0600

    Updated mainPage with filter

--- foodtruck/src/components/MainPage/MainPage.tsx
+++ foodtruck/src/components/MainPage/MainPage.tsx
@@ -1,18 +1,21 @@
-import './MainPage.css'
-import FilterForm from '../FilterForm/FilterForm'
-import { Trucks, TrucksProps } from '../Trucks/Trucks';
+import React from 'react';
+import './MainPage.css';
+import FilterForm from '../FilterForm/FilterForm';
+import { Trucks } from '../Trucks/Trucks';
 import { TruckData } from '../App/App';
 
 interface MainPageProps {
   truckData: TruckData[];
+  filter: (city: string) => void;
+  filteredTrucks: TruckData[];
+  reset: () => void;
 }
 
-export const MainPage: React.FC<MainPageProps> = ({ truckData }) => {
-  const trucksProps: TrucksProps = { truckData };
+export const MainPage: React.FC<MainPageProps> = ({ truckData, filter, filteredTrucks, reset }) => {
   return (
     <div className='truck-view'>
-      <FilterForm {...trucksProps} />
-      <Trucks {...trucksProps} />
+      <FilterForm truckData={truckData} filter={filter} filteredTrucks={filteredTrucks} reset={reset} />
+      <Trucks truckData={filteredTrucks.length ? filteredTrucks : truckData} />
     </div>
   );
 };
