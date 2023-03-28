0faace40f4ae0137a796e765af9f9128a2eba37f
Angie Staffieri <angie.staffieri@gmail.com>
2023-03-28 11:33:56 -0600

    truck card typescript corrections

--- foodtruck/src/components/TruckCard/TruckCard.tsx
+++ foodtruck/src/components/TruckCard/TruckCard.tsx
@@ -1,26 +1,40 @@
-import './TruckCard.css'
+import "./TruckCard.css";
 import { Link } from "react-router-dom";
 
-interface TruckCardProps {
-    id: number;
-    name: string;
-    city: string;
-    date: string;
-    cuisine: string;
-    website: string;
-    image: string;
-  }
-
-export const TruckCard: React.FC<TruckCardProps> = ({ id, name, city, date, cuisine, website, image }) => {
-    return (
-        <Link to={`/foodtruck/${name}`} style={{ textDecoration: "none", width: "300px", height: "400px" }}>
-            <div className='card-container' style={{ backgroundImage: `url(${image})`}}>
-                <div className='card-display'>
-                    <h3 className='truck-name'>{name}</h3>
-                    <p>{city}</p>
-                    <p>{date}</p>
-                </div>
-            </div>
-        </Link>
-    )
+export interface TruckCardProps {
+  id: number;
+  name: string;
+  city: string;
+  date: string;
+  cuisine: string;
+  website: string;
+  image: string;
 }
+
+export const TruckCard: React.FC<TruckCardProps> = ({
+  id,
+  name,
+  city,
+  date,
+  cuisine,
+  website,
+  image,
+}) => {
+  return (
+    <Link
+      to={`/foodtruck/${name}`}
+      style={{ textDecoration: "none", width: "300px", height: "400px" }}
+    >
+      <div
+        className="card-container"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <div className="card-display">
+          <h3 className="truck-name">{name}</h3>
+          <p>{city}</p>
+          <p>{date}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
