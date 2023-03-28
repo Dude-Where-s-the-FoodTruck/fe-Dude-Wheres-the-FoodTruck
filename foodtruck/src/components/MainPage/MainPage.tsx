d961b360a90623df53017e900b466b3279289dfa
Angie Staffieri <angie.staffieri@gmail.com>
2023-03-28 11:33:16 -0600

    mainpage typescript corrections

--- foodtruck/src/components/MainPage/MainPage.tsx
+++ foodtruck/src/components/MainPage/MainPage.tsx
@@ -1,39 +1,18 @@
 import './MainPage.css'
 import FilterForm from '../FilterForm/FilterForm'
 import { Trucks, TrucksProps } from '../Trucks/Trucks';
+import { TruckData } from '../App/App';
 
 interface MainPageProps {
-    truckData: {
-      data: {
-        id: number;
-        attributes: {
-          name: string;
-          cuisine_type: string;
-          web_link: string;
-          image_link: string;
-        };
-        relationships: {
-          type: string;
-          id: number;
-          attributes: {
-            event_date: string;
-            city: string;
-            latitude: number;
-            longitude: number;
-            start_time: string;
-            description: string;
-          };
-        }[];
-      }[];
-    }[];
-  }
-
-export const MainPage: React.FC<MainPageProps> = ({ truckData } ) => {
-    const trucksProps: TrucksProps = { truckData };
-    return (
-        <div className='truck-view'>
-            <FilterForm />
-            <Trucks {...trucksProps} />
-        </div>
-    )
+  truckData: TruckData[];
 }
+
+export const MainPage: React.FC<MainPageProps> = ({ truckData }) => {
+  const trucksProps: TrucksProps = { truckData };
+  return (
+    <div className='truck-view'>
+      <FilterForm />
+      <Trucks {...trucksProps} />
+    </div>
+  );
+};
