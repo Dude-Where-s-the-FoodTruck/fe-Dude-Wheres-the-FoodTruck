aac113cfc4c78acfff9c736074b74ba5454148a3
John Ammon <ammonj78@gmail.com>
2023-03-30 14:13:36 -0600

    Added handle click methods in LogIn component

--- foodtruck/src/components/LogIn/LogIn.tsx
+++ foodtruck/src/components/LogIn/LogIn.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-
 import './LogIn.css'
 
-const LogIn = () => {
+interface LogInProps{ 
+    setUserType: (userType: string) => void
+}
+
+export const LogIn: React.FC<LogInProps> = ({ setUserType }) => {
+
+    const handleUserClick = () => {
+        setUserType('user')
+    }
+
+    const handleOwnerClick = () => {
+        setUserType('owner')
+    }
+
     return(
         <div>
-            
+            <h1>Pick One!</h1>
+            <button onClick={handleUserClick}>I'm A hungry User</button>
+            <button onClick={handleOwnerClick}>I'm A Truck Owner</button>
         </div>
     )
 }
