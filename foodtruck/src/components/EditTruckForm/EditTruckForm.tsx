929cc09954fcb34e9aa32e4a571ec417609a72ba
jordan smith <jordan.smith6035@gmail.com>
2023-03-31 11:24:59 -0600

    Add edit truck class component shell

--- foodtruck/src/components/EditTruckForm/EditTruckForm.tsx
+++ foodtruck/src/components/EditTruckForm/EditTruckForm.tsx
@@ -1,1 +1,22 @@
 import './EditTruckForm.css'
+import React from 'react'
+
+
+interface EditTruckFormProps {}
+
+
+export class EditTruckForm extends React.Component<EditTruckFormProps> {
+    constructor(props: EditTruckFormProps) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
