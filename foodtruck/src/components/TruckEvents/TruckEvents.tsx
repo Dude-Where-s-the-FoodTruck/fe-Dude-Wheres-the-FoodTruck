7916521cdf9551d9f2d349c1708411115e5aeff6
jordan smith <jordan.smith6035@gmail.com>
2023-03-31 11:20:23 -0600

    Add truck event function shell

--- foodtruck/src/components/TruckEvents/TruckEvents.tsx
+++ foodtruck/src/components/TruckEvents/TruckEvents.tsx
@@ -1,1 +1,12 @@
 import './TruckEvents.css'
+import React from 'react'
+
+interface TruckEventProps {}
+
+export const TruckEvents: React.FC<TruckEventProps> = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
