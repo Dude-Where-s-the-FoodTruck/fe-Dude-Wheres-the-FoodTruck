7997a42d1f330c4de036597d060b31776947133a
jordan smith <jordan.smith6035@gmail.com>
2023-03-31 11:27:37 -0600

    Add create event form class shell

--- foodtruck/src/components/CreateEventForm/CreateEventForm.tsx
+++ foodtruck/src/components/CreateEventForm/CreateEventForm.tsx
@@ -1,1 +1,21 @@
 import './CreateEventForm.css'
+import React from 'react'
+
+
+interface CreateEventFormProps {}
+
+
+export class CreateEventForm extends React.Component<CreateEventFormProps> {
+    constructor(props: CreateEventFormProps) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
