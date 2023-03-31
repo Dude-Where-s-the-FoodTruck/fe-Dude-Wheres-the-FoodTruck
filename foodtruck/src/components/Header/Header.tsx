980bacb35c9fa6319131513e02ee8119d12b0243
Angie Staffieri <angie.staffieri@gmail.com>
2023-03-31 10:12:06 -0600

    fixed header link

--- foodtruck/src/components/Header/Header.tsx
+++ foodtruck/src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { Link } from 'react-router-dom'
 import headerLogo from '../../assets/foodtruck-logo.png'
 import './Header.css'
 
 export const Header = () => {
     return(
-        <header className="header">
-            <img className="truck-logo" src={headerLogo} alt='foodTruck logo'/>
-        </header>
+        <Link to="/main" style={{textDecoration: "none"}}>
+            <header className="header">
+                <img className="truck-logo" src={headerLogo} alt='foodTruck logo'/>
+            </header>
+        </Link>
     )
 }
