d48ba6932e7c394a7b2a1240f8f6b60143ed3321
Angie Staffieri <angie.staffieri@gmail.com>
2023-04-02 17:10:35 -0600

    fixed truckdetails so the information is pulling from the same place

--- foodtruck/cypress/e2e/eventDetails.cy.ts
+++ foodtruck/cypress/e2e/eventDetails.cy.ts
@@ -1,12 +1,16 @@
 describe('template spec', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.intercept("GET", "https://intense-thicket-16951.herokuapp.com/api/v1/food_trucks", {fixture: "example.json"})
+    cy.visit('http://localhost:3000/')
     cy.get('.user-button').contains("I'm A hungry User")
     .click()
-    cy.intercept("GET", "https://intense-thicket-16951.herokuapp.com/api/v1/food_trucks/2", {fixture: "eventDetails.json"})
-    cy.get(".trucks-container").children().first().click()
+    
   })
   it("Should have a truck name", () => {
-
+    cy.fixture('eventDetails.json').then((truckDetails) => {
+      cy.intercept('GET', "https://intense-thicket-16951.herokuapp.com/api/v1/food_trucks/2", truckDetails).as('getTruckDetails')
+      cy.get('.trucks-container').children().first().click()
+      cy.wait(500)
+    })
   })
 })
