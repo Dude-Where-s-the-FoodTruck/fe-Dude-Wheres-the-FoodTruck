8d54acc52f1134ba7819ee82788c6bcdebb9eff7
Angie Staffieri <angie.staffieri@gmail.com>
2023-04-02 14:23:01 -0600

    Login screen tests

--- foodtruck/cypress/e2e/login.cy.ts
+++ foodtruck/cypress/e2e/login.cy.ts
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('My First Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+  it('Should have two buttons', () => {
+    cy.get('.user-button').contains("I'm A hungry User")
+    cy.get('.owner-button').contains("I'm A Truck Owner")
+  })
+  it("Should have a user page", () => {
+    cy.get('.user-button').contains("I'm A hungry User")
+    .click()
+    cy.url().should('eq', 'http://localhost:3000/main')
+  })
+  it("Should have an owner page", () => {
+    cy.get('.owner-button').contains("I'm A Truck Owner")
+    .click()
+    cy.url().should('eq', 'http://localhost:3000/owner')
+  })
+  it("Should have an h1 tag on the main page", () => {
+    cy.get('h1').contains("Pick One!")
   })
 })
