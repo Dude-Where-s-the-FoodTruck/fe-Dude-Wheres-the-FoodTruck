5dca2c38edd868e6cc93a3d4668414fe1f189471
John Ammon <ammonj78@gmail.com>
2023-04-03 15:16:45 -0600

    Test form to edit event Not Passing

--- foodtruck/cypress/e2e/edit-event.cy.ts
+++ foodtruck/cypress/e2e/edit-event.cy.ts
@@ -16,33 +16,33 @@ describe('User view tests', () => {
     })
     
     
-    // it('date, start time, end time, description, street, city, state, zip',() => {
-    //     cy.get('#event_date').type('05/05/2023')
-    //     .should('have.value', '05/05/2023')
-    //     cy.get('#start_time').type('07:05 am')
-    //     .should('have.value', '07:05 am')
-    //     cy.get('#end_time').type('08:05 pm')
-    //     .should('have.value', '08:05 pm')
-    //     cy.get('#description').type('around the corner and up the way')
-    //     .should('have.value', 'around the corner and up the way')
-    //     cy.get('#street').type('123')
-    //     .should('have.value', '123')
-    //     cy.get('#city').type('Aurora')
-    //     .should('have.value', 'Aurora')
-    //     cy.get('#state').type('CO')
-    //     .should('have.value', 'CO')
-    //     cy.get('#zip').type('80011')
-    //     .should('have.value', '80011')
-    // })
+    it('date, start time, end time, description, street, city, state, zip',() => {
+        cy.get('#event_date').type('05/05/2023')
+        .should('have.value', '05/05/2023')
+        cy.get('#start_time').type('07:05 am')
+        .should('have.value', '07:05 am')
+        cy.get('#end_time').type('08:05 pm')
+        .should('have.value', '08:05 pm')
+        cy.get('#description').type('around the corner and up the way')
+        .should('have.value', 'around the corner and up the way')
+        cy.get('#street').type('123')
+        .should('have.value', '123')
+        cy.get('#city').type('Aurora')
+        .should('have.value', 'Aurora')
+        cy.get('#state').type('CO')
+        .should('have.value', 'CO')
+        cy.get('#zip').type('80011')
+        .should('have.value', '80011')
+    })
 
     // it('should have button UpdateEvent',() => {
 
     // })
 
-    it('should have button Back to home',() => {
-        cy.get('.back-to-owner').click()
-        cy.url().should("eq", "http://localhost:3000/owner")
-    })
+    // it('should have button Back to home',() => {
+    //     cy.get('.back-to-owner').click()
+    //     cy.url().should("eq", "http://localhost:3000/owner")
+    // })
 
     // it('should be able to patch event',() => {
     
