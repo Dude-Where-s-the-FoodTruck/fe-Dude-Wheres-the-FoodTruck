30432286dfe00924bd1920c90c59f67e488fa8d1
Angie Staffieri <angie.staffieri@gmail.com>
2023-04-02 19:21:13 -0600

    completed the before each

--- foodtruck/cypress/e2e/eventDetails.cy.ts
+++ foodtruck/cypress/e2e/eventDetails.cy.ts
@@ -4,13 +4,14 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000/')
     cy.get('.user-button').contains("I'm A hungry User")
     .click()
-    
-  })
-  it("Should have a truck name", () => {
     cy.fixture('eventDetails.json').then((truckDetails) => {
       cy.intercept('GET', "https://intense-thicket-16951.herokuapp.com/api/v1/food_trucks/2", truckDetails).as('getTruckDetails')
       cy.get('.trucks-container').children().first().click()
       cy.wait(500)
     })
+    
+  })
+  it("Should have a truck name", () => {
+    cy.get('h1').contains("The Food Truck We Deserve")
   })
 })
