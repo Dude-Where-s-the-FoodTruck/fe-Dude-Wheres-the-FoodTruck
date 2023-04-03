96e7c85b07cbc6928b1e7f81e3bef21e049d9727
John Ammon <ammonj78@gmail.com>
2023-04-03 14:49:58 -0600

    Test Add Event button tkes user to add event Form passing

--- foodtruck/cypress/e2e/ownerview.cy.ts
+++ foodtruck/cypress/e2e/ownerview.cy.ts
@@ -12,69 +12,69 @@ describe('User view tests', () => {
         cy.get('.footer').contains("© Dude, Where's The FoodTruck")
     })
 
-    // it('should have a button Add Event',() => {
-    //     cy.get('.add-event-button').contains('Add Event').click()
-    //     cy.url().should("eq", "http://localhost:3000/owner/create-event")
-    // })
+    it('should have a button Add Event',() => {
+        cy.get('.add-event-button').contains('Add Event').click()
+        cy.url().should("eq", "http://localhost:3000/owner/create-event")
+    })
 
-    // it(' should be able to type in edit truck name(input)',() => {
-    //     cy.get('.name-input').type('testing')
-    // })
+    it(' should be able to type in edit truck name(input)',() => {
+        cy.get('.name-input').type('testing')
+    })
 
-    // it('should be able to update truck website',() => {
-    //     cy.get('.website-input').type('https://i.etsystatic.com/7829877/r/il/78280b/743003426/il_1588xN.743003426_kg62.jpg')
-    // })
+    it('should be able to update truck website',() => {
+        cy.get('.website-input').type('https://i.etsystatic.com/7829877/r/il/78280b/743003426/il_1588xN.743003426_kg62.jpg')
+    })
 
-    // it(' should be able to add img for truck',() => {
-    //     cy.get('label').contains('Choose a file')
-    // })
+    it(' should be able to add img for truck',() => {
+        cy.get('label').contains('Choose a file')
+    })
 
-    // it("Should have options to select", () => {
-    //     cy.get('.select-type').contains("Spanish")
-    //     cy.get('.select-type').contains("French")
-    //     cy.get('.select-type').contains("Italian")
-    //     cy.get('.select-type').contains("Japanese")
-    //     cy.get('.select-type').contains("Greek")
-    //     cy.get('.select-type').contains("Vegan")
-    //     cy.get('.select-type').contains("Fusion")
-    //     cy.get('.select-type').contains("German")
-    // })
+    it("Should have options to select", () => {
+        cy.get('.select-type').contains("Spanish")
+        cy.get('.select-type').contains("French")
+        cy.get('.select-type').contains("Italian")
+        cy.get('.select-type').contains("Japanese")
+        cy.get('.select-type').contains("Greek")
+        cy.get('.select-type').contains("Vegan")
+        cy.get('.select-type').contains("Fusion")
+        cy.get('.select-type').contains("German")
+    })
 
-    // it(' should have a cancel button that clears the input fields',() => {
-    //     cy.get('.name-input').type('testing')
-    //     .should('have.value', 'testing')
-    //     cy.get('.website-input').type('website')
-    //     .should('have.value', 'website')
-    //     cy.get('.cancel-button').click()
-    //     cy.get('.cancel-button')
-    //     cy.get('.name-input').should('have.value', '')
-    //     cy.get('.website-input').should('have.value', '')
-    // })
+    it(' should have a cancel button that clears the input fields',() => {
+        cy.get('.name-input').type('testing')
+        .should('have.value', 'testing')
+        cy.get('.website-input').type('website')
+        .should('have.value', 'website')
+        cy.get('.cancel-button').click()
+        cy.get('.cancel-button')
+        cy.get('.name-input').should('have.value', '')
+        cy.get('.website-input').should('have.value', '')
+    })
 
-// // when updates are pulled check the post
-    // it('should have a submit button to patch updates',() => {
-    //     cy.get('.name-input').type('testing')
-    //     .should('have.value', 'testing')
-    //     cy.get('.website-input').type('website')
-    //     .should('have.value', 'website')
-    //     cy.get('.select-type').select('Spanish')
-    //     cy.get('.submit-button').click()
+// when updates are pulled check the post
+    it('should have a submit button to patch updates',() => {
+        cy.get('.name-input').type('testing')
+        .should('have.value', 'testing')
+        cy.get('.website-input').type('website')
+        .should('have.value', 'website')
+        cy.get('.select-type').select('Spanish')
+        cy.get('.submit-button').click()
 
-    // })
+    })
 
-    // it('should display all events for a single truck',() => {
-    //     cy.get('.truck-events-container')
-    //     .contains('Denver')
-    //     .contains('Truck 2')
-    // })
+    it('should display all events for a single truck',() => {
+        cy.get('.truck-events-container')
+        .contains('Denver')
+        .contains('Truck 2')
+    })
 
     it(' should be able to click an event to go to edit',() => {
         cy.get('.all-events-container > :nth-child(1)').click()
         cy.url().should("eq", "http://localhost:3000/owner/events/1")
     })
 
-    // it('should have a button Add Event',() => {
-        
-        
-    // })
+    it('should have a button Add Event',() => {
+        cy.get('.add-event-button').click()
+        cy.url().should("eq", "http://localhost:3000/owner/create-event")
+    })
 })
