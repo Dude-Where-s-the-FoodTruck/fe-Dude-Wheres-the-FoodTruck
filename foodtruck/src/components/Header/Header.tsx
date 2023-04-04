bbabb0e16a0734a81535885ed452cb9018be869e
jordan smith <jordan.smith6035@gmail.com>
2023-04-04 15:04:29 -0600

    Fix css for cypress

--- foodtruck/src/components/Header/Header.tsx
+++ foodtruck/src/components/Header/Header.tsx
@@ -4,10 +4,13 @@ import "./Header.css";
 
 export const Header = () => {
   return (
+    <>
+    
     <Link to="/main" style={{ textDecoration: "none" }}>
       <header className="user-header">
         <img className="truck-logo" src={headerLogo} alt="foodTruck logo" />
       </header>
     </Link>
+    </>
   );
 };
